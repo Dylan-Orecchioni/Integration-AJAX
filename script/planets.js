7fd1a004176664c05c7d4380ae8faa1707d3d999
Dylan <dylan77120@live.fr>
2023-10-27 01:44:17 +0200

    facto code + affichage detailplanet

--- script/planets.js
+++ script/planets.js
@@ -1,37 +1,98 @@
 const planetData = document.querySelector('#planetData');
 const paragraph = document.querySelector('.text-hidden');
 const maxplanet = document.querySelector('.countPlanets');
+const population = document.querySelector('.population');
+const diameter = document.querySelector('.diameter');
+const climate = document.querySelector('.climat');
+const gravity = document.querySelector('.gravity');
+const terrain = document.querySelector('.terrain');
+const name = document.querySelector('.name');
 
-async function getData(url) {
+onInit();
+
+async function onInit() {
+    const planets = await getAllPlanets();
+    displayPlanets(planets);
+    displayCount(planets);
+}
+
+async function getAllPlanets() {
+    const allPlanets = [];
+    let nextPage = "https://swapi.dev/api/planets/";
+
+    while(nextPage){
+        const planetsPerPage = await getPlanet(nextPage);
+        const planetResults = planetsPerPage.results;
+
+        allPlanets.push(...planetResults);
+
+        nextPage = planetsPerPage.next;
+    }
+
+    return {count: allPlanets.length, results: allPlanets};
+}
+
+async function getPlanet(url) {
     const response = await fetch(url);
     const data = await response.json();
 
     return data;
 }
 
-async function displayPlanets() {
-    const planets = await getData('https://swapi.dev/api/planets/');
-    const count = planets.count;
-    maxplanet.textContent = count + ' planètes';
+async function displayPlanets(planets) {
+  const planetsResults = planets.results;
 
-    for (let i = 1; i < Math.floor(count / 10); i++) {
-        const planetPerPage = await getData(`https://swapi.dev/api/planets/?page=${i}`);
-        const planetResults = planetPerPage.results;
+  planetsResults.forEach(planet => {
+      const planetItem = createPlanetItem(planet);
+      planetData.appendChild(planetItem);
 
-        planetResults.forEach((planetID) => {
-            const planetItem = document.createElement('tr');
-            planetItem.classList.add('planets-item');
-            planetItem.innerHTML = `
-                <td>${planetID.name}</td>
-                <td>${planetID.terrain}</td>
-            `;
-            planetData.appendChild(planetItem);
-        });
-    }
+      planetItem.addEventListener('click', () => {
+        displayPlanetsDetails(planet);
+      })
+  });
 
-    const sectionPlanets = document.querySelector('.section-planets');
-    sectionPlanets.classList.add('scrollable');
+  const sectionPlanets = document.querySelector('.section-planets');
+  sectionPlanets.classList.add('scrollable');
+
 }
 
-displayPlanets();
+function createPlanetItem(planete){
+    const planetItem = document.createElement('tr');
+
+    planetItem.classList.add('planets-item');
+    planetItem.innerHTML = `
+                <td>${planete.name}</td>
+                <td>${planete.terrain}</td>
+            `;
+    return planetItem;
+}
+
+function displayCount(planets){
+    maxplanet.textContent = planets.count + ' planètes';
+}
+
+
+
+function displayPlanetsDetails(planetDetails){
+    //J'efface les données precedentes
+    name.textContent = '';
+    diameter.textContent = '';
+    climate.textContent = '';
+    population.textContent = '';
+    terrain.textContent = '';
+    gravity.textContent = '';
+
+    // je met a jour les données de la planète
+    name.textContent = planetDetails.name;
+    population.textContent = planetDetails.population;
+    diameter.textContent = planetDetails.diameter;
+    climate.textContent = planetDetails.climate;
+    gravity.textContent = planetDetails.gravity;
+    terrain.textContent = planetDetails.terrain;
+}
+
+
+
+
+
 
--- script/script.js
+++ script/script.js
@@ -2,23 +2,26 @@ const persons = document.querySelector('.persons');
 const vehicles = document.querySelector('.vehicles');
 const planets = document.querySelector('.planets');
 
+onInit();
+
+async function onInit() {
+    const countPeople = await getCount('https://swapi.dev/api/people/');
+    const countVehicles = await getCount('https://swapi.dev/api/vehicles/');
+    const countPlanets = await getCount('https://swapi.dev/api/planets/');
+    displayCount(countPeople, countVehicles, countPlanets);
+}
+
 async function getCount(url) {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
 
     return data;
 }
 
-async function displayCount(){
-    const personCount = await getCount('https://swapi.dev/api/people/');
-    const vehiclesCount = await getCount('https://swapi.dev/api/vehicles/');
-    const planetsCount = await getCount('https://swapi.dev/api/planets/');
+async function displayCount(countPeople, countVehicles, countPlanets){
+    persons.textContent =  countPeople.count;
+    vehicles.textContent =  countVehicles.count;
+    planets.textContent =  countPlanets.count;
 
-    persons.textContent = `${personCount}`;
-    vehicles.textContent = `${vehiclesCount}`;
-    planets.textContent = `${planetsCount}`;
 }
-
-displayCount();
 
