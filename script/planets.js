76ce7047a4cd025c50742a0397eb1eea8ccd606a
Dylan <dylan77120@live.fr>
2023-10-26 11:48:35 +0200

    fix table

--- script/planets.js
+++ script/planets.js
@@ -1,4 +1,7 @@
 const planetData = document.querySelector('#planetData');
+const paragraph = document.querySelector('.text-hidden');
+const maxplanet = document.querySelector('.count');
+
 async function getCount(url) {
     const response = await fetch(url);
     const data = await response.json();
@@ -6,20 +9,32 @@ async function getCount(url) {
     return data;
 }
 
-
 async function displayPlanets(){
     const planet = (await getCount('https://swapi.dev/api/planets/')).count;
+    maxplanet.textContent = planet + ' planètes';
 
-    for(let i = 1; i < planet && i < 10; i++){
-
+    for(let i = 1; i < planet ; i++){
         const planetID = await getCount(`https://swapi.dev/api/planets/${i}/`);
         planetData.innerHTML += `
-            <tr>
+            <tr class="planets-item">
                 <td>${planetID.name}</td>
                 <td>${planetID.terrain}</td>
             </tr>
             `;
-    }
-}
 
+        const sectionPlanets = document.querySelector('.section-planets');
+        if (planet[i] === 10) {
+            sectionPlanets.classList.add('scrollable');
+        }
+    }
+
+
+    // planets.forEach(planet => {
+    //     planet.addEventListener('click', () => {
+    //         paragraph.textContent = planet.textContent;
+    //     })
+    // })
+
+}
 displayPlanets();
+
